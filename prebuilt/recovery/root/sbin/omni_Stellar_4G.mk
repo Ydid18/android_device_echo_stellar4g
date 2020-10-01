91b490f52d573cab39a7a4a37a0a274932b38508
Ydid18 <72167686+Ydid18@users.noreply.github.com>
2020-10-01 20:49:55 +0200

    Add files via upload

---
+++ prebuilt/recovery/root/sbin/omni_Stellar_4G.mk
@@ -0,0 +1,34 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+#$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace $$DEVICE$$ with your Device Name's Value.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/ECHO/Stellar_4G/prebuilt/zImage:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := Stellar_4G
+PRODUCT_NAME := omni_Stellar_4G
+PRODUCT_BRAND := ECHO
+PRODUCT_MODEL := Stellar_4G
+PRODUCT_MANUFACTURER := ECHO
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Add fingerprint from Stock ROM build.prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    # These lines are from my device. You MUST Replace yours.
+    BUILD_FINGERPRINT="ECHO/Stellar_4G/Stellar_4G:7.0/NRD90M/20180109.182643:user/release-keys" \
+    PRIVATE_BUILD_DESC="full_n390b-user 7.0 NRD90M 1515492843 release-keys"
